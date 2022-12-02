d28dace7314efaa1cd9f7b7af8a0feae7f049db0
Anatoliy <antononanatoliy82@gmail.com>
2022-12-02 23:37:54 +0500

    feat: add solution task create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import  fs  from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const text = 'I am fresh and young';
 const create = async () => {
-    // Write your code here 
+    const path_link = path.resolve(__dirname + '/files' + '/fresh.txt');
+    fs.stat(path_link, (error) => {
+        if (error) {
+            fs.open(path_link, 'w', (error) => {
+                if (error) throw new Error ('File not create');                
+            });            
+        } else {
+            throw new Error ('FS operation failed');
+        }
+        fs.writeFile(path_link, text, (error) => {
+            if(error) throw error;
+        });
+    });
 };
 
 await create();
