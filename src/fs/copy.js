00b8c21fde357e8b83ee6441f4506dafce37c1c1
Anatoliy <antononanatoliy82@gmail.com>
2022-12-03 22:42:04 +0500

    feat: add solution for task copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
-const copy = async () => {
-    // Write your code here 
-};
+import  fs  from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const copy = async () => {
+
+    const path_link = path.join(__dirname, 'files');
+    const path_link_all = path.resolve(__dirname);
+    const copy_file = path.join(path_link_all, 'files_copy');
+
+
+        fs.stat(path_link, (error) => {
+            if (error) throw new Error ('FS operation failed');
+            fs.mkdir(copy_file, (error) => {
+                if (error) throw new Error ('FS operation failed');
+                });
+            fs.readdir(path_link, (error, files) => {
+                if (error) throw new Error ('!!!');
+                files.forEach(file => {
+                    fs.copyFile(path.join(path_link, file), path.join(copy_file, file), (error) => {
+                        if (error) throw new Error ('FS operation failed');
+                    });
+                });       
+            });
+        });
+}
 copy();
