d5688daf0778f16bd3bf6a714b9e1cba5dfaccba
Anatoliy <antononanatoliy82@gmail.com>
2022-12-04 18:45:42 +0500

    feat: add solution for env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+    const object_variable_environment = process.env;
+    let arr = [];
+    Object.keys(object_variable_environment).forEach(function(element) {
+        if (element.slice(0, 4) === 'RSS_') {
+            arr.push(element + '=' + object_variable_environment[element]);
+        }
+    });
+    console.log(arr.join('; '));
+}
 
 parseEnv();
