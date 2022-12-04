a2da46baad7084f70ef59a233bed2608554de7a7
Anatoliy <antononanatoliy82@gmail.com>
2022-12-04 14:38:30 +0500

    feat: add solution for list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
-const list = async () => {
-    // Write your code here 
+import  fs  from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const path_link = path.join(__dirname, 'files');
+
+const list = async () => {    
+    fs.readdir(path_link, (error, files) => {
+        if (error) throw new Error ('FS operation failed');
+        console.log(files);
+    });
 };
 
 await list();
