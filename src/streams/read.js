1edc769bf39359e31f1368240b626e0d07d74d41
Anatoliy <antononanatoliy82@gmail.com>
2022-12-05 21:21:57 +0500

    feat: add solution for read.js stream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import  fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file_for_read = path.join(__dirname, 'files', 'fileToRead.txt');
+
+
 const read = async () => {
-    // Write your code here 
+    const read_stream = fs.ReadStream(file_for_read);
 };
 
 await read();
