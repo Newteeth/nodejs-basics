7165bfd310b6299864848a93767a329e98d1b18c
Anatoliy <antononanatoliy82@gmail.com>
2022-12-05 23:18:01 +0500

    fix: delete console.log on the solution write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -7,7 +7,6 @@ const __dirname = path.dirname(__filename);
 const file_for_write = path.join(__dirname, 'files', 'fileToWrite.txt');
 
 const write = async () => {
-    console.log(file_for_write);
     const write_strim = fs.createWriteStream(file_for_write, { encoding: 'utf8' });
     process.stdin.pipe(write_strim, { end: false });
 };
