c71246533c2e4795b21fcc333af932f4b37975af
Anatoliy <antononanatoliy82@gmail.com>
2022-12-05 20:08:43 +0500

    refactor: add array in solution for args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,11 +1,13 @@
 const parseArgs = () => {    
     const command_line_arguments = process.argv;
+    let arr = [];
 
     for (let elem = 0; elem < command_line_arguments.length; elem++) {
         if (command_line_arguments[elem].slice(0, 2) === '--') {
-            console.log(command_line_arguments[elem] + ' is ' + command_line_arguments[elem + 1]);
+            arr.push((command_line_arguments[elem]).slice(2) + ' is ' + command_line_arguments[elem + 1]);
         }
     }
+    console.log(arr.join(', '));
 };
 
 parseArgs();
