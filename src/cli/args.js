8c296ad3d1fe635b43aff67ef778a005046e4e52
Anatoliy <antononanatoliy82@gmail.com>
2022-12-05 17:02:30 +0500

    feat: add solution for args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
-const parseArgs = () => {
-    // Write your code here 
+const parseArgs = () => {    
+    const command_line_arguments = process.argv;
+
+    for (let elem = 0; elem < command_line_arguments.length; elem++) {
+        if (command_line_arguments[elem].slice(0, 2) === '--') {
+            console.log(command_line_arguments[elem] + ' is ' + command_line_arguments[elem + 1]);
+        }
+    }
 };
 
 parseArgs();
