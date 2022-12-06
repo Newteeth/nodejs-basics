709db5925bbe5aea1e1bab5aa3f3b01d3c2d1ba9
Anatoliy <antononanatoliy82@gmail.com>
2022-12-06 22:21:02 +0500

    feat: add solution for transform.js task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+
+import path from 'path';
+import { Transform } from 'stream';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const transform = async () => {
-    // Write your code here 
-};
+    const word_reverse = new Transform({
+        transform(chunk, encoding, callback){
+            const word = chunk.toString().split('').reverse().join('');
+            callback(null, word);
+        }
+    });
+    process.stdin.pipe(word_reverse, { end: false }).pipe(process.stdout, { end: true });
+}
 
 await transform();
+
